1b1f1fefa245d2a61770777de7a2c4bc92182e31
John.Schneider <john.schneider@centurycommunities.com>
2026-05-11 12:46:07 -0400

    removed builder.io

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,6 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       { protocol: "https", hostname: "placehold.co" },
-      { protocol: "https", hostname: "cdn.builder.io" },
       { protocol: "https", hostname: "*.public.blob.vercel-storage.com" },
     ],
   },
